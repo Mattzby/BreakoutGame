19d7c5205d7271a24a644bc8ff2804f0c9f20d60
Matt <mattzby@gmail.com>
2017-11-29 18:16:16 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float movementSpeed;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		//float moveVertical = Input.GetAxis("Vertical");
+
+		Vector2 movement = new Vector3 (moveHorizontal, 0.0f);
+
+		rb.velocity = movement * movementSpeed;			
+	}
+}
