6358a6c796c005e165eadba50a6b666a9048aaa3
Matt <mattzby@gmail.com>
2017-12-12 15:18:45 -0500

    Added scoreboard to gameover screen. No name entry. No online connectivity.

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -6,19 +6,32 @@ using UnityEngine.SceneManagement;
 
 public class GameOverController : MonoBehaviour {
 	public Text scoreText;
+	public Text instructionsText;
+
+	private int playerScore;
 
 	// Use this for initialization
 	void Start () {
-		int score = GameController.getInstance ().GetScore ();
-		scoreText.text = "Score: " + score;	
+
+		//get score from gamecontroller and display
+		if (GameController.getInstance () != null) {
+			playerScore = GameController.getInstance ().GetScore ();
+		}
+		else {
+			playerScore = 0;
+		}
+		scoreText.text = "Score: " + playerScore;
 
 		//Cleanup GameController to reset score/lives for next playthrough
-		Destroy (GameObject.FindGameObjectWithTag("GameController"));
+		//Destroy (GameObject.FindGameObjectWithTag("GameController"));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton ("Fire1")) {
+		bool highScoreInputActivated = true;
+		//bool highScoreInputActivated = GameObject.Find("HighScoreTable").GetComponent<HighScoreController>().PlayerIsTyping();
+
+		if (Input.GetButton ("Fire1") && !highScoreInputActivated) {
 			SceneManager.LoadScene ("Title", LoadSceneMode.Single);			
 		}	
 	}
---
+++ Assets/Scripts/HighScoreController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreController : MonoBehaviour {
+
+	public GameObject highScoreRow;
+	public GameObject scoreText;
+
+	private List<HighScore> highScores;
+	private int playerScore;
+	private bool nameEntryActivated;
+	private bool highScoreAchieved;
+
+	void Start () {
+		nameEntryActivated = false;
+		highScores = new List<HighScore> ();
+
+		//prepopulated highscores
+		highScores.Add(new HighScore("MRC",70000));
+		highScores.Add(new HighScore("LOL",50000));
+		highScores.Add(new HighScore("WTF",45000));
+		highScores.Add(new HighScore("XYZ",30000));
+		highScores.Add(new HighScore("NUB",10000));
+
+		if (GameController.getInstance () != null) {
+			playerScore = GameController.getInstance ().GetScore ();
+		}
+		else {
+			playerScore = 0;
+		}
+
+		highScoreAchieved = EvaluatePlayerScore(playerScore);
+		GenerateHighScoreRows();
+
+		//Cleanup GameController to reset score/lives for next playthrough
+		Destroy (GameObject.FindGameObjectWithTag("GameController"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void GenerateHighScoreRows() {	
+		int rank = 1;
+		foreach (HighScore hs in highScores) {
+			if (rank < 6) {
+				GameObject currentRow = Instantiate (highScoreRow, transform) as GameObject;
+				Text rankText = currentRow.transform.GetChild (0).GetComponent<Text>();
+				Text nameText = currentRow.transform.GetChild (1).GetComponent<Text>();
+				Text scoreText = currentRow.transform.GetChild (2).GetComponent<Text>();
+
+				rankText.text = rank.ToString();
+				scoreText.text = hs.score.ToString();
+				
+				//check if null - if so, that means new highscore and requires player input
+				if (hs.initials != null) {
+					nameText.text = hs.initials;
+				} else {
+					//TODO: Allow user to input name/initials when achieving high score
+					nameText.text = "YOU";
+					rankText.color = Color.red;
+					scoreText.color = Color.red;
+					nameText.color = Color.red;
+					//nameText.gameObject.GetComponent<Text> ().enabled = false;
+					//nameText.gameObject.GetComponent<InputField> ().enabled = true;
+				}
+			}
+			rank++;			
+		}
+	}
+
+	bool EvaluatePlayerScore(int playerScore) {
+		int index = 0;
+		foreach (HighScore hs in highScores) {
+			if (playerScore > hs.score) {
+				highScores.Insert(index, new HighScore(null, playerScore));
+				return true;
+			}
+			index++;
+		}
+		return false;
+	}
+
+	//Class that stores existing high scores
+	private class HighScore {
+		public string initials;
+		public int score;
+
+		public HighScore(string i, int s){
+			initials = i;
+			score = s;
+		}
+	}
+
+	public bool PlayerIsTyping() {
+		return nameEntryActivated;
+	}
+}
